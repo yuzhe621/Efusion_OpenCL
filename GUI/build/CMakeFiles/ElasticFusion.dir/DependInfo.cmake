5884890505f0b2b24c1072adc99dd804180dbee4
yuzhe621 <yangyuzhe621@gmail.com>
2018-08-26 01:21:02 +0100

    Signed-off-by: yuzhe621 <yangyuzhe621@gmail.com>

---
+++ GUI/build/CMakeFiles/ElasticFusion.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/src/Main.cpp" "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/build/CMakeFiles/ElasticFusion.dir/Main.cpp.o"
+  "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/src/MainController.cpp" "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/build/CMakeFiles/ElasticFusion.dir/MainController.cpp.o"
+  "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/src/Tools/GroundTruthOdometry.cpp" "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/build/CMakeFiles/ElasticFusion.dir/Tools/GroundTruthOdometry.cpp.o"
+  "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/src/Tools/LiveLogReader.cpp" "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/build/CMakeFiles/ElasticFusion.dir/Tools/LiveLogReader.cpp.o"
+  "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/src/Tools/OpenNI2Interface.cpp" "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/build/CMakeFiles/ElasticFusion.dir/Tools/OpenNI2Interface.cpp.o"
+  "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/src/Tools/RawLogReader.cpp" "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/build/CMakeFiles/ElasticFusion.dir/Tools/RawLogReader.cpp.o"
+  "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/src/Tools/RealSenseInterface.cpp" "/home/peggy/Downloads/ElasticFusion-master_CL/GUI/build/CMakeFiles/ElasticFusion.dir/Tools/RealSenseInterface.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "linux=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/peggy/Pangolin/build/Pangolin/include"
+  "/home/peggy/Pangolin/build/Pangolin/build/src/include"
+  "/usr/include/eigen3"
+  "/home/peggy/Downloads/ElasticFusion-master_CL/Core/src"
+  "/home/peggy/Downloads/ElasticFusion-master_CL/deps/OpenNI2/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
